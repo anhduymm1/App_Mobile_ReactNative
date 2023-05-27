b88c97794de2152ed6ee92e26e2993cbff6a0b01
anhduymm1 <duybui100073@gmail.com>
2023-05-27 14:09:21 +0700

    commit lan 2

--- Camera.jsx
+++ Camera.jsx
@@ -7,6 +7,7 @@ function Camera() {
   const [scanned, setScanned] = useState(false);
   const [hasPermission, setHasPermission] = useState(null);
   const [text,setText] = useState('Not yet Scan')
+  const [acivate, setActive] = useState(true)
 
   // const getBarCodeScannerPermissions = async () => {
   //   const { status } = await QRCodeScanner.requestPermissionsAsync();
@@ -18,25 +19,30 @@ function Camera() {
   // }, []);
 
   const handleBarCodeScanned = async ({ type, data }) => {
+    await setActive(false)
     await setScanned(true)
-    await setText(data)
-    await console.log(data)
-  };
+    console.log(data)
+  }
+ 
+  pressButton = async () =>{
+    await setActive(true)
+    await setScanned(false)
+  }
+
 
 
   return (
     <View style={styles.container}>
       <QRCodeScanner
         showMarker={true}
-        onRead={scanned==false ? handleBarCodeScanned : undefined }
-        
+        onRead={ handleBarCodeScanned}
+        reactivate={acivate}
         camerastyle={styles.camera}
         topViewStyle={styles.topView}
         bottomViewStyle={styles.bottomView}
         markerStyle={styles.marker}
       />
-      <Text>{text}</Text>
-     {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+     {scanned && <Button title={'Tap to Scan Again'} onPress={pressButton } />}
     </View>
   );
 }
